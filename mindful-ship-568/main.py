3b99161ef5da17ce7fd2e8733746b9b67200ee9c
solomreb <solomreb@onid.oregonstate.edu>
2015-10-02 20:51:15 -0700

    attempting to get weather

--- mindful-ship-568/main.py
+++ mindful-ship-568/main.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import cgi
+import pywapi
+import string
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <h3>Enter your zip to get weather</h3>
+    <form action="/weather" method="post">
+      <div><input type="text" pattern="\d*" name="zip"></div>
+      <div><input type="submit" value="Get Weather"></div>
+    </form>
+  </body>
+</html>
+"""
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(MAIN_PAGE_HTML)
+
+class Weather(webapp2.RequestHandler):
+    def post(self):
+#	weather_com_result = pywapi.get_weather_from_weather_com(self.request.get('zip'))	
+        self.response.write('<html><body>Weather.com says it is:<pre>')
+#	self.response.write(string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "\n\n"
+	self.response.write('</pre></body></html>')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/weather', Weather),
 ], debug=True)
